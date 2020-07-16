4291aa3dd383a43615450ee90d3441c632139060
wandalen <wandalen.me@gmail.com>
2020-07-16 14:55:41 +0300

    .

--- proto/wtools/amid/l5/censor.test/Censor.test.s
+++ proto/wtools/amid/l5/censor.test/Censor.test.s
@@ -8,8 +8,8 @@ if( typeof module !== 'undefined' )
   _.include( 'wTesting' );
 }
 
-var _global = _global_;
-var _ = _global_.wTools;
+let _global = _global_;
+let _ = _global_.wTools;
 
 // --
 // tests
@@ -24,7 +24,7 @@ function basic( test )
 // test suite definition
 // --
 
-var Self =
+let Self =
 {
 
   name : 'Tools.mid.Censor',
