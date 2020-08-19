20e456b818fd5eb2d0b0ff51dc034cf932718f42
Jack Shell <jackshel45@gmail.com>
2020-08-19 17:08:24 +0300

    Adding tests for `fileReplace` routine

--- proto/wtools/amid/l5/censor.test/Censor.test.s
+++ proto/wtools/amid/l5/censor.test/Censor.test.s
@@ -12,12 +12,58 @@ let _global = _global_;
 let _ = _global_.wTools;
 
 // --
+// context
+// --
+
+function onSuiteBegin()
+{
+  let context = this;
+  context.suiteTempPath = _.path.tempOpen( _.path.join( __dirname, '../..' ), 'censor' );
+  context.assetsOriginalPath = _.path.join( __dirname, '_asset' );
+  context.appJsPath = _.path.nativize( _.module.resolve( 'wCensor' ) );
+}
+
+//
+
+function onSuiteEnd()
+{
+  let context = this;
+  _.assert( _.strHas( context.suiteTempPath, '/censor' ) )
+  _.path.tempClose( context.suiteTempPath );
+}
+
+// --
 // tests
 // --
 
 function basic( test )
 {
 
+}
+
+//
+
+function replaceBasic( test )
+{
+  let context = this;
+  let a = test.assetFor( 'basic' );
+  a.reflect();
+
+  a.ready.then( ( op ) =>
+  {
+    test.case = 'test';
+    var options =
+    {
+      filePath : a.abs( 'before/**' ),
+      ins : 'line',
+      sub : 'abc',
+    }
+    var got = _.fileReplace( options )
+
+    return null;
+  } );
+
+  return a.ready;
 }
 
 // --
@@ -31,10 +77,22 @@ let Self =
   silencing : 1,
   enabled : 1,
 
+  onSuiteBegin,
+  onSuiteEnd,
+  routineTimeOut : 300000,
+
+  context :
+  {
+    suiteTempPath : null,
+    assetsOriginalPath : null,
+    appJsPath : null,
+  },
+
   tests :
   {
 
     basic,
+    replaceBasic,
 
   }
 
